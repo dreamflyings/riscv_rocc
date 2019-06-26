dd3d209ceeb7ee8740e0ab6c1cae486089362115
root <psigourney@utexas.edu>
2019-06-26 21:09:12 +0000

    Adding RSA code

--- isa-sim/rsa/rsa.h
+++ isa-sim/rsa/rsa.h
@@ -8,7 +8,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <math.h>
 
 class rsa_t : public rocc_t
 {
@@ -21,10 +21,150 @@ public:
   {
   }
 
+  //Find number of base2 values in key (exponent) needed for lookupArray
+  int findExpBase2ElemCount(uint64_t exponent)
+  {
+	int counter = 0;
+	for(uint64_t i = 1; i < exponent; i *= 2)
+		counter += 1;
+	//printf("findExpBase2ElemCount.counter: %d \n", counter);
+	return counter;
+  }
+
+  //Create a lookup array containing: "all base-2 powers mod modulus of the plaintext char"
+  uint64_t* createLookupArray(int num_elems, int plainChar, uint64_t modulus)
+  { 
+	uint64_t* results = (uint64_t*)malloc(num_elems * sizeof(uint64_t));	
+	results[0] = (uint64_t)plainChar % modulus;
+	for(int i = 1; i < num_elems; i++)
+	{
+		results[i] = ((uint64_t)pow(results[i-1], 2)) % modulus;
+		//printf("createLookupArray.results[%d]: %ld \n", i, results[i]);
+	}
+
+	return results;
+  } 
+  
+  //Create an array containing the binary representation of the key (pub/pri exponent)
+  bool* createBinaryExpArray(uint64_t exponent)
+  {
+	bool* results = (bool*)malloc(64 * sizeof(bool)); //exponent is a 64bit number
+	uint64_t mask = 1 << 63;  //Must instantiate as 64bit value for compiler to allocate 64bits
+	mask = 1;
+	for(int i = 0; i < 64; i++)
+	{
+		if((exponent & mask) > 0)
+			results[i] = 1;
+		else
+			results[i] = 0;
+
+		//printf("createBinaryExpArray.results[%d]: %d \n", i, results[i]);
+		mask = mask << 1;
+	}
+	return results;
+  }
+
+  //Use the lookupArray and binaryExpArray to calculate the cipher value of the plainChar
+  uint64_t combineCalculatedValues(uint64_t* lookupArray, int lookupArraySize, bool* binaryKeyArray, uint64_t modulus)
+  {
+	uint64_t total_coeff = 1 << 63; //Init to a 64bit value
+	total_coeff = 1;
+	for(int i = 0; i < lookupArraySize; i++)
+		if(binaryKeyArray[i] == 1)
+		{
+			//printf("combineCalculatedValues.lookupArray[%d]: %ld \n", i, lookupArray[i]);
+			total_coeff = (total_coeff * lookupArray[i]) % modulus;
+		}
+	//printf("combineCalculatedValues.total_coeff mod modulus: %ld mod %ld \n", total_coeff, modulus);
+	return total_coeff % modulus;
+  }
+
+  uint64_t encrypt(char plain, uint64_t pubExp, uint64_t modulus)
+  {
+//	printf("%c is %d, ", plain, (int)plain);
+	int pow_counter = findExpBase2ElemCount(pubExp);
+	uint64_t* lookupArray = createLookupArray(pow_counter, plain, modulus);
+	bool* binPubExp = createBinaryExpArray(pubExp);
+	uint64_t result = combineCalculatedValues(lookupArray, pow_counter, binPubExp, modulus);
+//	printf("encrypted: %ld \n", result);
+	return result;
+
+  }
+
+  char decrypt(uint64_t cipher, uint64_t priExp, uint64_t modulus)
+  {
+	int pow_counter = findExpBase2ElemCount(priExp);
+	uint64_t* lookupArray = createLookupArray(pow_counter, cipher, modulus);
+	bool* binPriExp = createBinaryExpArray(priExp);
+	uint64_t charValue = combineCalculatedValues(lookupArray, pow_counter, binPriExp, modulus);
+//	printf("charValue: %ld = %c \n", charValue, char(charValue));
+
+	return (char)charValue;
+  }
+
+
   reg_t custom0(rocc_insn_t insn, reg_t xs1, reg_t xs2)
   {
     switch (insn.funct)
     {
+      case 0: //Encrypt; receive pubExp and modulus
+        pubExp_reg = xs1;
+        pubExp = p->get_mmu()->load_uint64(pubExp_reg);
+
+        mod_reg = xs2;
+        modulus = p->get_mmu()->load_uint64(mod_reg);
+	
+        break;
+
+      case 1: //Encrypt; receive plaintext and text size
+        text_size = xs2;
+        plaintext = (unsigned char*) malloc(text_size * sizeof(char));
+        for(int i = 0; i < text_size; i++)
+                plaintext[i] = p->get_mmu()->load_uint8(xs1 + i);
+
+        break;
+
+
+      case 2: //Encrypt; receive addr for ciphertext and text size and send ciphertext back
+        ciphertext_reg = xs1;
+
+	for(int i = 0; i < text_size; i++)
+	{
+		p->get_mmu()->store_uint64(ciphertext_reg+(i * sizeof(uint64_t)), encrypt(plaintext[i], pubExp, modulus));
+		if(plaintext[i] == '\0')
+			break;
+	}
+	free(plaintext);
+        break;
+
+
+      case 3: //Decrypt; receive priExp and modulus
+        priExp_reg = xs1;
+        priExp = p->get_mmu()->load_uint64(priExp_reg);
+
+        mod_reg = xs2;
+        modulus = p->get_mmu()->load_uint64(mod_reg);
+
+	break;
+
+
+      case 4: //Decrypt; receive ciphertext and text size
+	text_size = xs2;
+	encrypted = (uint64_t*)malloc(text_size * sizeof(uint64_t));
+	
+	for(int i = 0; i < text_size; i++)
+		encrypted[i] = (uint64_t)p->get_mmu()->load_uint64(xs1 + (i * sizeof(uint64_t)));
+	
+	break;
+
+
+      case 5: //Decrypt; receive address for decrypted results and send them back
+
+	for(int i = 0; i < text_size; i++)
+		p->get_mmu()->store_uint8(xs1+(i*sizeof(uint8_t)), (uint8_t)decrypt(encrypted[i], priExp, modulus));
+	
+	free(encrypted);
+	break;
 
       default:
         illegal_instruction();
@@ -32,6 +172,18 @@ public:
 
     return -1; // accelerator currently returns nothing
   }
+private:
+  reg_t pubExp_reg;
+  reg_t priExp_reg;
+  reg_t mod_reg;
+  unsigned char* plaintext;
+  uint64_t* encrypted;
+  reg_t ciphertext_reg;
+  int text_size;
+
+  uint64_t priExp;
+  uint64_t pubExp;
+  uint64_t modulus;
 };
 REGISTER_EXTENSION(rsa, []() { return new rsa_t; })
 #endif
--- tests/rsa-rocc.c
+++ tests/rsa-rocc.c
@@ -16,6 +16,40 @@ typedef struct uint128 {
   uint64_t lo;
 } uint128;
 
+//Stolen from: https://stackoverflow.com/questions/19738919/gcd-function-for-c/45434118
+int gcd(int a, int b)
+{
+    int temp;
+    while (b != 0)
+    {
+        temp = a % b;
+        a = b;
+        b = temp;
+    }
+    return a;
+}
+
+//Stolen from: https://stackoverflow.com/questions/27695656/c-pseudo-rsa-solving-for-d-decryption-key-quickly-with-large-numbers
+uint64_t extended_gcd(uint64_t a, uint64_t b)
+{
+    uint64_t x = 0, lastx = 1, y = 1, lasty = 0, temp, quotient;
+    while(b != 0)
+    {
+        temp = b;
+        quotient = a / b;
+        b = a % b;
+        a = temp;
+        temp = x;
+        x = lastx - quotient * x;
+        lastx = temp;
+        temp = y;
+        y = lasty - quotient * y;
+        lasty = temp;
+   }
+   return lasty;
+}
+
+
 int main() {
 /* Private-Key: (128 bit)                                                                                                                                         */
 /* modulus: */
@@ -32,13 +66,39 @@ int main() {
 /* coefficient: 10155582946292377246 (0x8cefe0e210c5a69e) */
 
     //DO NOT MODIFY
-    uint128 modulus = {0xe037d35a8b160eb7LL,  0xf11919bfef440917LL};
-    uint128 privateExp = {0x00cab10ccaa4437b67LL,  0x11c977a277fe00a1LL};
-    uint64_t pubExp = 65537;
+    //uint128 modulus = {0xe037d35a8b160eb7LL,  0xf11919bfef440917LL};
+    //uint128 privateExp = {0x00cab10ccaa4437b67LL,  0x11c977a277fe00a1LL};
+    //uint64_t pubExp = 65537;
     const char plaintext[] = "Hello !";
-    uint128 ciphertext;
-    uint128 decrypted;
+    //uint128 ciphertext;
+    //uint128 decrypted;
     //END DO NOT MODIFY
+
+    uint64_t prime1 = 7103;
+    uint64_t prime2 = 8821;
+    uint64_t modulus = prime1 * prime2;
+    uint64_t totient = (prime1 - 1) * (prime2 - 1);
+    uint64_t pubExp = 65537;
+
+    assert(gcd(pubExp, prime1-1) == 1);
+    assert(gcd(pubExp, prime2-1) == 1);
+
+    uint64_t priExp;
+    int64_t tempPriExp = extended_gcd(totient, pubExp);
+    if(tempPriExp < 0)  priExp = tempPriExp + totient;  //Ensure priExp is positive
+    else priExp = tempPriExp;
+
+    int text_size = sizeof(plaintext);
+    uint64_t ciphertext[text_size];
+    uint64_t decrypted;
+
+    printf("prime1:  %ld \n", prime1);
+    printf("prime2:  %ld \n", prime2);
+    printf("modulus: %ld \n", modulus);
+    printf("totient: %ld \n", totient);
+    printf("pubExp:  %ld \n", pubExp);
+    printf("priExp:  %ld \n\n", priExp);
+
     int dummy_result;
 
     uint128 mymod = {0xe037d35a8b160eb7LL, 0xf11919bfef440917};
@@ -46,7 +106,16 @@ int main() {
     uint64_t initCycle, duration;
     initCycle = rdcycle();
     asm volatile ("fence"); //NOTE that fences are only needed if your accelerator accesses memory
+
     /* YOUR CODE HERE: Invoke your RSA acclerator, write the encrypted output of plaintext to ciphertext */
+    //Send publicExp and modulus
+    ROCC_INSTRUCTION(0, dummy_result, &pubExp, &modulus, 0);
+
+    //Send plaintext and text size
+    ROCC_INSTRUCTION(0, dummy_result, &plaintext, sizeof(plaintext), 1);
+
+    //Send address for ciphertext results
+    ROCC_INSTRUCTION(0, dummy_result, &ciphertext, sizeof(plaintext), 2);
     asm volatile ("fence");
 
     //DO NOT MODIFY
@@ -55,7 +124,16 @@ int main() {
     initCycle = rdcycle();
     //END DO NOT MODIFY
 
+
     /* YOUR CODE HERE: Invoke your RSA acclerator, write the decrypted output of ciphertext to decrypted */
+    //Send privateExp and modulus
+    ROCC_INSTRUCTION(0, dummy_result, &priExp, &modulus, 3);
+
+    //Send ciphertext and text size
+    ROCC_INSTRUCTION(0, dummy_result, &ciphertext, sizeof(plaintext), 4);
+
+    //Send address for decrypted results
+    ROCC_INSTRUCTION(0, dummy_result, &decrypted, sizeof(plaintext), 5);
     asm volatile ("fence");
 
     //DO NOT MODIFY
